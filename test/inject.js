3661e8187b552f995e27a3a9296ec717adeb12e1
bredele <olivier.wietrich@gmail.com>
2014-03-29 23:06:08 -0600

    add tests for inject and mixin

--- test/inject.js
+++ test/inject.js
@@ -1,10 +1,26 @@
 var plumby = require('..');
 var assert = require('assert');
 
-describe('Inject', function() {
+describe('Inject > ', function() {
 
 	it('has inject handler', function() {
 		assert.equal(typeof plumby.inject, 'function');
+	});
+
+	describe('Mixin > ', function() {
+
+		it('injects an object with an app ', function(done) {
+			plumby.inject('foo', {
+				custom: done,
+				name: 'olivier'
+			});
+
+			var app = plumby('foo');
+			assert(app.custom);
+			assert.equal(app.name, 'olivier');
+			app.custom();
+		});
+		
 	});
 	
 });
