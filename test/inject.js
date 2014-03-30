1110b3e4a1fd44ca2b38a72cead21a65bbed9d31
bredele <olivier.wietrich@gmail.com>
2014-03-29 23:40:27 -0600

    inject and use plugins

--- test/inject.js
+++ test/inject.js
@@ -3,38 +3,55 @@ var assert = require('assert');
 
 describe('Inject > ', function() {
 
-	it('has inject handler', function() {
-		assert.equal(typeof plumby.inject, 'function');
-	});
+  it('has inject handler', function() {
+    assert.equal(typeof plumby.inject, 'function');
+  });
 
-	describe('Mixin > ', function() {
+  describe('Mixin > ', function() {
 
-		it('injects an object with an app ', function(done) {
-			plumby.inject('foo', {
-				custom: done,
-				name: 'olivier'
-			});
+    it('injects an object with an app ', function(done) {
+      plumby.inject('foo', {
+        custom: done,
+        name: 'olivier'
+      });
 
-			var app = plumby('foo');
-			assert(app.custom);
-			assert.equal(app.name, 'olivier');
-			app.custom();
-		});
+      var app = plumby('foo');
+      assert(app.custom);
+      assert.equal(app.name, 'olivier');
+      app.custom();
+    });
 
-		it("doesn't overrid plumby app handlers", function(done) {
-			var used = false;
-			plumby.inject('bar', {
-				custom: done,
-				use: function() {
-					used = true;
-				}
-			});
-			var app = plumby('bar');
-			app.use(function() {
-				if(!used) done();
-			});
-		});
-		
-	});
-	
+    it("doesn't overridw plumby app handlers", function(done) {
+      var used = false;
+      plumby.inject('bar', {
+        custom: done,
+        use: function() {
+          used = true;
+        }
+      });
+      var app = plumby('bar');
+      app.use(function() {
+        if(!used) done();
+      });
+    });
+    
+  });
+
+  describe('Plugins', function() {
+
+    it('injects an app with plugins', function() {
+      plumby
+        .inject('hello')
+        .use(function(ctx) {
+          ctx.hello = 'world';
+        });
+
+      var app = plumby('hello');
+      assert.equal(app.hello, 'world');
+    });
+
+    //NOTE: we should be able to catch the created event!
+    
+  });
+
 });
